7991993b7e6ce25f00657d3f8dbb654243fadadd
preeth25 <preetham2524@gmail.com>
2025-12-05 13:52:55 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,8 @@
+import java.util.*;
+ public class Area{
+    public static void main(String[] args){
+        float radius = 7.5f;
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+}
