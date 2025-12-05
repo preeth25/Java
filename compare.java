98176c19ac67b48dc9cf849d990912e89d9bdf72
preeth25 <preetham2524@gmail.com>
2025-12-05 14:46:22 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class compare {
+    public static void Compare(int a[]) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++)
+                if (a[i] == a[j]) {
+                    count++;
+                }
+
+        }
+
+        System.out.println(" there are : " + count + " commen elements");
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of an array :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the array element:");
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+
+        }
+
+        Compare(a);
+
+    }
+}
