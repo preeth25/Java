9dc2721e9185ac22809f35590d3efeb53ad758eb
Preetham <preetham2524@gmail.com>
2025-12-05 16:08:53 +0530

    Add files via upload

---
+++ ArrayUsingLoop.java
@@ -0,0 +1,14 @@
+public class ArrayUsingLoop {
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        boolean found = false;
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] == 1 && arr[i + 1] == 2 && arr[i + 2] == 3) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found);
+    }
+    
+}
